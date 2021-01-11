b9f0d14d1b31d319a7947ec083b097a7518e85fd
Jonathan Fisher <jpfisher72@gmail.com>
2021-01-11 11:50:00 -0800

    finished adding form functionality, working afaik

--- src/Form.js
+++ src/Form.js
@@ -17,6 +17,11 @@ class Form extends Component {
         })
     }
 
+    submitForm = () => {
+        this.props.handleSubmit(this.state)
+        this.setState(this.initialState)
+    }
+
     render() {
         const { name, job } = this.state;
 
@@ -36,6 +41,10 @@ class Form extends Component {
                     id="job"
                     value={job}
                     onChange={this.handleChange} />
+                <input
+                    type = "button"
+                    value = "Submit"
+                    onClick={this.submitForm} />
             </form>
         )
     }
